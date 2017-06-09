596cf2da67b98d798b905feb0ecd295de40881cb
VenHayz <theepictoastguy666@gmail.com>
2017-06-08 23:29:07 -0700

    First progress

---
+++ Test/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int denars = (int)numericUpDown1.Value;
+            int* addr = int.Parse(textBox1.Text);
+        }
+    }
+}
---
+++ WPMTK/WPMTK.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPMTK
+{
+    public class Windows
+    {
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+    }
+
+    public class Process
+    {
+        public IntPtr hWnd { get; }
+        Process(string window_title)
+        {
+            hWnd = Windows.FindWindow(null, window_title);
+        }
+    }
+}
