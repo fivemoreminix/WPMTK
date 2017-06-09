ff095c8b9aa9d374c7fffa85d399b0b2d7f5cf21
VenHayz <theepictoastguy666@gmail.com>
2017-06-09 01:36:27 -0700

    test trainer major improvements

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -33,7 +33,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
@@ -48,15 +52,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Denars";
+            this.label1.Text = "integral value to set to";
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(138, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.Size = new System.Drawing.Size(37, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "set";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,30 +74,62 @@
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "0x0000000";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(210, 15);
+            this.label2.Location = new System.Drawing.Point(192, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.Size = new System.Drawing.Size(121, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "address for currency";
+            this.label2.Text = "address for manipulation";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(319, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(193, 70);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.ReadOnly = true;
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "current value at address";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 62);
+            this.ClientSize = new System.Drawing.Size(366, 100);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
             this.Name = "Form1";
-            this.Text = "Mount&Blade Trainer";
+            this.Text = "WPMTK Test Trainer";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +142,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -12,6 +12,8 @@ namespace Test
 {
     public partial class Form1 : Form
     {
+        private IntPtr address;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,11 +26,31 @@ namespace Test
 
         private unsafe void addressParse(object sender, EventArgs e) {
             int denars = (int)numericUpDown1.Value;
-            int* addr = (int*) int.Parse(textBox1.Text);
+            
         }
 
         private void Form1_Load(object sender, EventArgs e) {
 
+        }
+
+        // set the address
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                address = (IntPtr)int.Parse(textBox1.Text);
+                button2.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("\"" + textBox1.Text + "\" is not recognized as a valid memory address.", "Address Error!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (button2.Enabled != true)
+                button2.Enabled = true;
         }
     }
 }
