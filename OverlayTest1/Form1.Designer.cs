e342f658ac58e0b84662f9a43dee2a17220fb7cc
MasterUmbra <FelixerPotion@gmail.com>
2017-06-09 12:25:52 +0700

    Added the GDI Overlay test project.

---
+++ OverlayTest1/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace OverlayTest1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.overlayCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // overlayCheckBox
+            // 
+            this.overlayCheckBox.AutoSize = true;
+            this.overlayCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.overlayCheckBox.Name = "overlayCheckBox";
+            this.overlayCheckBox.Size = new System.Drawing.Size(62, 17);
+            this.overlayCheckBox.TabIndex = 0;
+            this.overlayCheckBox.Text = "Overlay";
+            this.overlayCheckBox.UseVisualStyleBackColor = true;
+            this.overlayCheckBox.CheckedChanged += new System.EventHandler(this.overlayCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.overlayCheckBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox overlayCheckBox;
+    }
+}
+
---
+++ OverlayTest1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverlayTest1 {
+    public partial class Form1 : Form {
+        FormOverlay frm;
+        public Form1() {
+            frm = new FormOverlay();
+            InitializeComponent();
+        }
+
+        private void overlayCheckBox_CheckedChanged(object sender, EventArgs e) {
+            if (overlayCheckBox.Checked) {
+                frm.Show();
+            } else {
+                frm.Hide();
+            }
+
+        }
+    }
+}
