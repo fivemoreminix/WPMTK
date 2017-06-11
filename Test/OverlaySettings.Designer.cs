0e7a88b281d23d0947328f47be6113e817da78ff
VenHayz <theepictoastguy666@gmail.com>
2017-06-11 14:24:30 -0700

    some graphics for Test and (soon) better overlay tests

--- Test/OverlaySettings.Designer.cs
+++ Test/OverlaySettings.Designer.cs
@@ -29,15 +29,33 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OverlaySettings));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.borderlessBox = new System.Windows.Forms.CheckBox();
             this.showBox = new System.Windows.Forms.CheckBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.coordinatesStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.outlinedBoxCheck = new System.Windows.Forms.CheckBox();
+            this.filledBoxCheck = new System.Windows.Forms.CheckBox();
+            this.outlinedCircleCheck = new System.Windows.Forms.CheckBox();
+            this.filledCircleCheck = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.posShapeOne = new System.Windows.Forms.Label();
+            this.posShapeTwo = new System.Windows.Forms.Label();
+            this.posShapeThree = new System.Windows.Forms.Label();
+            this.posShapeFour = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -46,7 +64,7 @@
             this.groupBox1.Controls.Add(this.showBox);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(140, 125);
+            this.groupBox1.Size = new System.Drawing.Size(140, 179);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "General";
@@ -54,6 +72,7 @@
             // borderlessBox
             // 
             this.borderlessBox.AutoSize = true;
+            this.borderlessBox.Enabled = false;
             this.borderlessBox.Location = new System.Drawing.Point(7, 43);
             this.borderlessBox.Name = "borderlessBox";
             this.borderlessBox.Size = new System.Drawing.Size(74, 17);
@@ -73,43 +92,168 @@
             this.showBox.UseVisualStyleBackColor = true;
             this.showBox.CheckedChanged += new System.EventHandler(this.showBox_CheckedChanged);
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Location = new System.Drawing.Point(158, 13);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(170, 124);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Shape Test";
-            // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.coordinatesStatus});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 153);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 194);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(340, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // coordinatesStatus
             // 
             this.coordinatesStatus.Name = "coordinatesStatus";
-            this.coordinatesStatus.Size = new System.Drawing.Size(272, 17);
-            this.coordinatesStatus.Text = "Overlay not active - check \"show\" box to enable it.";
+            this.coordinatesStatus.Size = new System.Drawing.Size(402, 17);
+            this.coordinatesStatus.Text = "Overlay not active - check \"show\" box, and select some shapes to enable it.";
             // 
             // timer1
             // 
             this.timer1.Interval = 150;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.posShapeFour);
+            this.groupBox2.Controls.Add(this.posShapeThree);
+            this.groupBox2.Controls.Add(this.posShapeTwo);
+            this.groupBox2.Controls.Add(this.posShapeOne);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.pictureBox4);
+            this.groupBox2.Controls.Add(this.filledCircleCheck);
+            this.groupBox2.Controls.Add(this.pictureBox3);
+            this.groupBox2.Controls.Add(this.outlinedCircleCheck);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.filledBoxCheck);
+            this.groupBox2.Controls.Add(this.outlinedBoxCheck);
+            this.groupBox2.Location = new System.Drawing.Point(159, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(213, 178);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Draw Random Shapes";
+            // 
+            // outlinedBoxCheck
+            // 
+            this.outlinedBoxCheck.AutoSize = true;
+            this.outlinedBoxCheck.Location = new System.Drawing.Point(7, 27);
+            this.outlinedBoxCheck.Name = "outlinedBoxCheck";
+            this.outlinedBoxCheck.Size = new System.Drawing.Size(15, 14);
+            this.outlinedBoxCheck.TabIndex = 0;
+            this.outlinedBoxCheck.UseVisualStyleBackColor = true;
+            this.outlinedBoxCheck.CheckedChanged += new System.EventHandler(this.outlinedBoxCheck_CheckedChanged);
+            // 
+            // filledBoxCheck
+            // 
+            this.filledBoxCheck.AutoSize = true;
+            this.filledBoxCheck.Location = new System.Drawing.Point(7, 65);
+            this.filledBoxCheck.Name = "filledBoxCheck";
+            this.filledBoxCheck.Size = new System.Drawing.Size(15, 14);
+            this.filledBoxCheck.TabIndex = 2;
+            this.filledBoxCheck.UseVisualStyleBackColor = true;
+            // 
+            // outlinedCircleCheck
+            // 
+            this.outlinedCircleCheck.AutoSize = true;
+            this.outlinedCircleCheck.Location = new System.Drawing.Point(7, 103);
+            this.outlinedCircleCheck.Name = "outlinedCircleCheck";
+            this.outlinedCircleCheck.Size = new System.Drawing.Size(15, 14);
+            this.outlinedCircleCheck.TabIndex = 4;
+            this.outlinedCircleCheck.UseVisualStyleBackColor = true;
+            // 
+            // filledCircleCheck
+            // 
+            this.filledCircleCheck.AutoSize = true;
+            this.filledCircleCheck.Location = new System.Drawing.Point(7, 141);
+            this.filledCircleCheck.Name = "filledCircleCheck";
+            this.filledCircleCheck.Size = new System.Drawing.Size(15, 14);
+            this.filledCircleCheck.TabIndex = 6;
+            this.filledCircleCheck.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Test.Properties.Resources.RedCircleFilled;
+            this.pictureBox4.Location = new System.Drawing.Point(28, 133);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Test.Properties.Resources.RedCircleOutlined;
+            this.pictureBox3.Location = new System.Drawing.Point(28, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Test.Properties.Resources.RedBoxFilled;
+            this.pictureBox2.Location = new System.Drawing.Point(28, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // posShapeOne
+            // 
+            this.posShapeOne.AutoSize = true;
+            this.posShapeOne.Location = new System.Drawing.Point(67, 19);
+            this.posShapeOne.Name = "posShapeOne";
+            this.posShapeOne.Size = new System.Drawing.Size(29, 13);
+            this.posShapeOne.TabIndex = 9;
+            this.posShapeOne.Text = "POS";
+            // 
+            // posShapeTwo
+            // 
+            this.posShapeTwo.AutoSize = true;
+            this.posShapeTwo.Location = new System.Drawing.Point(67, 57);
+            this.posShapeTwo.Name = "posShapeTwo";
+            this.posShapeTwo.Size = new System.Drawing.Size(29, 13);
+            this.posShapeTwo.TabIndex = 10;
+            this.posShapeTwo.Text = "POS";
+            // 
+            // posShapeThree
+            // 
+            this.posShapeThree.AutoSize = true;
+            this.posShapeThree.Location = new System.Drawing.Point(67, 95);
+            this.posShapeThree.Name = "posShapeThree";
+            this.posShapeThree.Size = new System.Drawing.Size(29, 13);
+            this.posShapeThree.TabIndex = 11;
+            this.posShapeThree.Text = "POS";
+            // 
+            // posShapeFour
+            // 
+            this.posShapeFour.AutoSize = true;
+            this.posShapeFour.Location = new System.Drawing.Point(67, 133);
+            this.posShapeFour.Name = "posShapeFour";
+            this.posShapeFour.Size = new System.Drawing.Size(29, 13);
+            this.posShapeFour.TabIndex = 12;
+            this.posShapeFour.Text = "POS";
+            // 
             // OverlaySettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 175);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(384, 216);
             this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -119,6 +263,12 @@
             this.groupBox1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,9 +279,21 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckBox borderlessBox;
         private System.Windows.Forms.CheckBox showBox;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel coordinatesStatus;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox outlinedBoxCheck;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.CheckBox filledCircleCheck;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox outlinedCircleCheck;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox filledBoxCheck;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label posShapeFour;
+        private System.Windows.Forms.Label posShapeThree;
+        private System.Windows.Forms.Label posShapeTwo;
+        private System.Windows.Forms.Label posShapeOne;
     }
 }
--- Test/OverlaySettings.cs
+++ Test/OverlaySettings.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Timers;
 using System.Windows.Forms;
+using System.Drawing;
 using WOTK;
 
 namespace Test
 {
     public partial class OverlaySettings : Form
     {
+        Random rand;
         WPMTK.Process process; // process
         Overlay overlay; // overlay object
 
@@ -13,6 +16,11 @@ namespace Test
         public OverlaySettings(WPMTK.Process process)
         {
             InitializeComponent();
+            rand = new Random(DateTime.Now.Second * DateTime.Now.Millisecond);
+            posShapeOne.Text = "";
+            posShapeTwo.Text = "";
+            posShapeThree.Text = "";
+            posShapeFour.Text = "";
 
             try
             {
@@ -33,7 +41,7 @@ namespace Test
             overlay.IsOverlayShown(showBox.Checked);
             timer1.Enabled = showBox.Checked;
             if (!showBox.Checked)
-                coordinatesStatus.Text = "Overlay not active - check \"show\" box to enable it.";
+                coordinatesStatus.Text = "Overlay not active - check \"show\" box, and select some shapes to enable it.";
         }
 
         // borderless
@@ -52,6 +60,19 @@ namespace Test
             string coord = "Coordinates(X: " + bounds.X + ", Y: " + bounds.Y +
                 ", W: "+ bounds.Width + ", H: " + bounds.Height + ")";
             coordinatesStatus.Text = coord;
+        }
+
+        // outlined box
+        private void outlinedBoxCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            RectangleF rect = new RectangleF(
+                rand.Next(overlay.FormOverlay.Left, overlay.FormOverlay.Right),
+                rand.Next(overlay.FormOverlay.Top, overlay.FormOverlay.Bottom),
+                100,
+                100);
+            overlay.AddShape(Shapes.Rectangle, rect);
+            posShapeOne.Text = "X: " + rect.X + ", Y: " + rect.Y +
+                ", W: " + rect.Width + ", H: " + rect.Height;
         }
     }
 }
