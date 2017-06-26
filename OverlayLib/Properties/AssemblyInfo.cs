402dc5b89c164126557ddbc6e60b71ece0069eb0
VenHayz <theepictoastguy666@gmail.com>
2017-06-26 13:43:02 -0700

    Overlay and Process library assembly information

--- OverlayLib/Properties/AssemblyInfo.cs
+++ OverlayLib/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WOTK")]
+[assembly: AssemblyProduct("Windows Overlay Toolkit")]
 [assembly: AssemblyCopyright("Copyright © sistemiinferno 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
--- WPMTK/Properties/AssemblyInfo.cs
+++ WPMTK/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +7,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WPMTK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Process Manipulation Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("sistemiinferno")]
-[assembly: AssemblyProduct("WPMTK")]
+[assembly: AssemblyProduct("Windows Process Manipulation Toolkit")]
 [assembly: AssemblyCopyright("Copyright © sistemiinferno 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
