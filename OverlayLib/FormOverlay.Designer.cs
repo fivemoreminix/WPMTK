ecb7581b037984cfb48e9c6d68644e10e81f9795
VenHayz <theepictoastguy666@gmail.com>
2017-07-09 19:06:38 -0700

    drawing rate increase for overlay

--- OverlayLib/FormOverlay.Designer.cs
+++ OverlayLib/FormOverlay.Designer.cs
@@ -30,7 +30,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 250;
+            this.timer1.Interval = 25;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FormOverlay
